2507e0e07f1e3d46f1b34a530c6b37c317156c3b
Adil saeed <AdiL@Manjeets-MacBook-Air.local>
2021-07-25 22:53:23 +0530

    Added Javadoc.

--- src/main/java/com/adil/assetmanagement/dao/AssetRepository.java
+++ src/main/java/com/adil/assetmanagement/dao/AssetRepository.java
@@ -3,6 +3,11 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.adil.assetmanagement.dao.model.Asset;
 
+/**
+ * this will expose  crud operation for assets in the organisation.
+ * @author AdiL
+ *
+ */
 public interface AssetRepository extends CrudRepository<Asset, String> {
 	
 }
--- src/main/java/com/adil/assetmanagement/service/AssetCategoryService.java
+++ src/main/java/com/adil/assetmanagement/service/AssetCategoryService.java
@@ -4,13 +4,31 @@ import java.util.List;
 
 import com.adil.assetmanagement.model.Category;
 
+/**
+ * This service will take of asset category life-cycle.
+ * @author AdiL
+ *
+ */
 public interface AssetCategoryService {
 
 	
+
+	/**
+	 * this function will add category to the system.
+	 * @param category
+	 */
 	public void addCategory(Category category);
 
+	/**
+	 * this function will return all category from the system.
+	 * @param category
+	 */
 	public List<Category> getCategory();
 
+	/**
+	 * this function will update category to the system.
+	 * @param category
+	 */
 	public void updateCategory( String id, Category category);
 	
 }
